c8b7a1137559fc4cd371044c8ccde325f640b12a
Dror Simon <dror.simon@gmail.com>
2020-03-05 15:39:28 +0200

    Added GPU implementation for Sinkohrn. Added typing hints.

--- generate_morph.py
+++ generate_morph.py
@@ -5,7 +5,7 @@ import argparse
 import os
 from os import listdir
 from os.path import basename, isfile, join
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 import torch
@@ -18,6 +18,7 @@ from torchvision.utils import save_image
 import utils
 from pix2pix.models import networks
 from utils import get_jpg_images
+from dcgan_models import Generator, Encoder
 
 
 parser = argparse.ArgumentParser()
@@ -53,7 +54,16 @@ def _generate_metric(height, width, grid):
             i += 1
     return C
 
-def generate_metric(im_size):
+def generate_metric(im_size: Tuple[int]) -> np.ndarray:
+    """
+    Computes the Euclidean distances matrix
+    
+    Arguments:
+        im_size {Tuple[int]} -- Size of the input image (height, width)
+    
+    Returns:
+        np.ndarray -- distances matrix
+    """
     grid = np.meshgrid(*[range(x) for x in im_size])
     grid = np.stack(grid,-1)
     return _generate_metric(im_size[0], im_size[1], grid)
@@ -77,7 +87,24 @@ def generate_interpolation(height, width, plan, t):
     c = c[:height,:width] #* (I1_count*(1-t) + I2_count*t)
     return c
 
-def sinkhorn(a, b, C, height, width, epsilon, threshold=1e-7):
+def sinkhorn(a: np.ndarray, b: np.ndarray, C: np.ndarray, height: int, width: int, 
+             epsilon: float, threshold: float=1e-7) -> np.ndarray:
+    """Computes the sinkhorn algorithm naively, using the CPU.
+    
+    Arguments:
+        a {np.ndarray} -- the first distribution (image), normalized, and shaped to a vector of size height*width.
+        b {np.ndarray} -- the second distribution (image), normalized, and shaped to a vector of size height*width.
+        C {np.ndarray} -- the distances matrix
+        height {int} -- image height
+        width {int} -- image width
+        epsilon {float} -- entropic regularization parameter
+    
+    Keyword Arguments:
+        threshold {float} -- convergence threshold  (default: {1e-7})
+    
+    Returns:
+        np.ndarray -- the entropic regularized transportation plan, pushing distribution a to b.
+    """
     K = np.exp(-C/epsilon)
     v = np.random.randn(*a.shape)
     i = 0
@@ -97,7 +124,89 @@ def sinkhorn(a, b, C, height, width, epsilon, threshold=1e-7):
     P = P.reshape(height, width, height, width)
     return P
 
-def project_on_generator(G, pix2pix, target_image, E, dcgan_img_size=64, pix2pix_img_size=128):
+def sinkhorn_gpu(a: np.ndarray, b: np.ndarray, C: np.ndarray, height: int, width: int, 
+                 epsilon: float, threshold: float=1e-7) -> np.ndarray:
+    """Computes the sinkhorn algorithm using convolutional Wassestein distances (Solomon et al.), using the GPU.
+    
+    Arguments:
+        a {np.ndarray} -- the first distribution (image), normalized, and shaped to a vector of size height*width.
+        b {np.ndarray} -- the second distribution (image), normalized, and shaped to a vector of size height*width.
+        C {np.ndarray} -- the distances matrix
+        height {int} -- image height
+        width {int} -- image width
+        epsilon {float} -- entropic regularization parameter
+    
+    Keyword Arguments:
+        threshold {float} -- convergence threshold  (default: {1e-7})
+    
+    Returns:
+        np.ndarray -- the entropic regularized transportation plan, pushing distribution a to b.
+    """
+    K = torch.tensor(np.exp(-C/epsilon), dtype=torch.double).cuda()
+
+    # Compute the kernel:
+    kernel_size = max(int(epsilon*2),7)  # seems to be sufficient
+    if kernel_size % 2 == 0:
+        kernel_size += 1
+    X = torch.tensor(range(kernel_size)).repeat(kernel_size,1)
+    Y = torch.tensor(range(kernel_size)).reshape(-1,1).repeat(1,kernel_size)
+    D = torch.zeros_like(X)
+    for i in range(kernel_size):
+        for j in range(kernel_size):
+            D[i,j] = (X[i,j] - X[kernel_size//2,kernel_size//2])**2 + (Y[i,j] - Y[kernel_size//2,kernel_size//2])**2
+    k = torch.exp(-D.type(torch.double)/epsilon).cuda().reshape(1,1,kernel_size,kernel_size)
+    
+    a = torch.tensor(a.reshape(1,1,height,width), dtype=torch.double).cuda()
+    a /= a.sum()
+    b = torch.tensor(b.reshape(1,1,height,width), dtype=torch.double).cuda()
+    b /= b.sum()
+    v = torch.ones_like(a) / k.sum()
+    i = 0
+    a_mask = a==0
+    b_mask = b==0
+    last_convergence = np.inf
+
+    # Compute convolutional Sinkhorn
+    while True:
+        u = a/(torch.nn.functional.conv2d(v, k, padding=kernel_size//2))
+        u[a_mask] = 0  # Fix 0/0 devision
+        v = b/(torch.nn.functional.conv2d(u, k, padding=kernel_size//2))
+        v[b_mask] = 0  # Fix 0/0 devision
+        i += 1
+        if i % 10 == 0:
+            convergence = (torch.sum(u.reshape(-1, 1) * K * v.reshape(1,-1), dim=1) - a.reshape(-1)).pow(2).sum().item()  # Could probably be computed using convs as well
+            print(i, convergence)
+            if convergence < threshold or np.abs(last_convergence - convergence) < threshold:
+                break
+            last_convergence = convergence
+
+    # Compute the trasportation plan
+    P = u.reshape(-1, 1) * K * v.reshape(1,-1)  # Could probably be computed using convs as well
+    P = P.reshape(height, width, height, width).cpu().numpy().astype(np.float)
+    return P
+
+def project_on_generator(G: Generator, pix2pix: networks.UnetGenerator, 
+                         target_image: np.ndarray, E: Encoder, dcgan_img_size: int=64, 
+                         pix2pix_img_size: int=128) -> Tuple[np.ndarray, torch.Tensor]:
+    """Projects the input image onto the manifold span by the GAN. It operates as follows:
+    1. reshape and normalize the image
+    2. run the encoder to obtain a latent vector
+    3. run the DCGAN generator to obtain a low resolution image
+    4. run the Pix2Pix model to obtain a high resulution image
+    
+    Arguments:
+        G {Generator} -- DCGAN generator
+        pix2pix {networks.UnetGenerator} -- Low resolution to high resolution Pix2Pix model
+        target_image {np.ndarray} -- The image to project
+        E {Encoder} -- The DCGAN encoder
+    
+    Keyword Arguments:
+        dcgan_img_size {int} -- Low resolution image size (default: {64})
+        pix2pix_img_size {int} -- High resolution image size (default: {128})
+    
+    Returns:
+        Tuple[np.ndarray, torch.Tensor] -- The projected high resolution image and the latent vector that was used to generate it.
+    """
     # reshape and normalize image
     target_image = torch.Tensor(target_image).cuda().reshape(1,3,pix2pix_img_size,pix2pix_img_size)
     target_image = F.interpolate(target_image, scale_factor=dcgan_img_size/pix2pix_img_size, mode='bilinear')
@@ -115,7 +224,19 @@ def project_on_generator(G, pix2pix, target_image, E, dcgan_img_size=64, pix2pix
     out_image = utils.denorm(pix_outputs.detach()).clamp(0,1).cpu().numpy().reshape(3,-1,1)
     return out_image, z
 
-def preprocess_Q(Q, max_val=None, Q_counts=None):
+def preprocess_Q(Q: np.ndarray, max_val: float=None, Q_counts: np.ndarray=None) -> Tuple[np.ndarray, float, np.ndarray]:
+    """ Preprocess (normalize) input images before computing their barycenters
+    
+    Arguments:
+        Q {np.ndarray} -- Input images. Every image should reshaped to a column in Q.
+    
+    Keyword Arguments:
+        max_val {float} -- The maximum value. Should be changed from None when using the iterative algorithm (more than 1 iteration in the Algorithm) (default: {None})
+        Q_counts {np.ndarray} -- The sum of all the pixel values in each image. Should be changed from None when using the iterative algorithm (more than 1 iteration in the Algorithm) (default: {None})
+    
+    Returns:
+        Tuple[np.ndarray, float, np.ndarray] -- The normalized images the total maximum value and sum of pixels in each image
+    """
     if max_val is None:
         max_val = Q.max()
     Q = max_val - Q
@@ -124,7 +245,15 @@ def preprocess_Q(Q, max_val=None, Q_counts=None):
     Q = Q / Q_counts
     return Q, max_val, Q_counts
 
-def load_pix2pix(model_path='networks/zap50k_pix2pix'):
+def load_pix2pix(model_path: str='networks/zap50k_pix2pix') -> networks.UnetGenerator:
+    """Loads the Pix2Pix model
+    
+    Keyword Arguments:
+        model_path {str} -- Path to the trained Pix2Pix model (default: {'networks/zap50k_pix2pix'})
+    
+    Returns:
+        networks.UnetGenerator -- An object that holds the Pix2Pix conditional generator model
+    """
     netG = networks.define_G(3, 3, 64, 'unet_128', 'batch', True, 'normal', 0.02, [0]).module
     netG.load_state_dict(torch.load(model_path, map_location='cuda:0'))
     netG.eval()
@@ -132,13 +261,39 @@ def load_pix2pix(model_path='networks/zap50k_pix2pix'):
         param.requires_grad = False
     return netG
 
-def load_models():
+def load_models() -> Tuple[Generator, Encoder, networks.UnetGenerator]:
+    """ Load the generative models
+    
+    Returns:
+        Tuple[Generator, Encoder, networks.UnetGenerator] -- the DCGAN generator, its respective encoder and the Pix2Pix model
+    """
     generator = utils.load_generator(args.dcgan_latent_size, args.dcgan_num_filters, join(args.models_save_dir,'generator'))
     encoder = utils.load_encoder(args.dcgan_latent_size, args.dcgan_num_filters, join(args.models_save_dir,'encoder'))
     pix2pix = load_pix2pix(join(args.models_save_dir, args.dataset_name+"_pix2pix"))
     return generator, encoder, pix2pix
 
-def morph_project_only(im1, im2, Generator, Encoder, pix2pix, epsilon=20.0, L=9, dcgan_size=64, pix2pix_size=128, simulation_name="image_interpolation", results_path="results"):
+def morph_project_only(im1: np.ndarray, im2: np.ndarray, Generator: Generator, Encoder: Encoder, 
+                       pix2pix: networks.UnetGenerator, epsilon: float=20.0, L: int=9, dcgan_size: int=64, 
+                       pix2pix_size: int=128, simulation_name: str="image_interpolation", results_path: str="results") -> None:
+    """Generates 3 morphing processes given two images. 
+    The first is simple Wasserstein Barycenters, the second is our algorithm and 
+    the third is a simple GAN latent space linear interpolation
+    
+    Arguments:
+        im1 {np.ndarray} -- source image
+        im2 {np.ndarray} -- destination image
+        Generator {Generator} -- DCGAN generator (latent space to pixel space)
+        Encoder {Encoder} -- DCGAN encoder (pixel space to latent space)
+        pix2pix {networks.UnetGenerator} -- pix2pix model trained to increase an image resolution
+    
+    Keyword Arguments:
+        epsilon {float} -- entropic regularization parameter (default: {20.0})
+        L {int} -- number of images in the trasformation (default: {9})
+        dcgan_size {int} -- DCGAN image size (low resolution) (default: {64})
+        pix2pix_size {int} -- Pix2Pix image size (high resolution) (default: {128})
+        simulation_name {str} -- name of the simulation. Affects the saved file names (default: {"image_interpolation"})
+        results_path {str} -- the path to save the results in (default: {"results"})
+    """
     img_size = im1.shape[:2]
     im1, im2 = (I.transpose(2,0,1).reshape(3,-1,1) for I in (im1, im2))
 
@@ -186,7 +341,19 @@ def morph_project_only(im1, im2, Generator, Encoder, pix2pix, epsilon=20.0, L=9,
     save_image(torch.cat([out_OT,out_ours,out_GAN], dim=0), output_path, nrow=L, normalize=False, scale_each=False, range=(0,1))
     print(f"Image saved in {output_path}")
 
-def load_images(path1, path2, im_size=128):
+def load_images(path1: str, path2: str, im_size: int=128) -> Tuple[np.ndarray, np.ndarray, str]:
+    """Loads images
+    
+    Arguments:
+        path1 {str} -- path to the first image
+        path2 {str} -- path to the second image
+    
+    Keyword Arguments:
+        im_size {int} -- the desired image size (default: {128})
+    
+    Returns:
+        Tuple[np.ndarray, np.ndarray, str] -- returns the two images and a the files' names (used later to save to results)
+    """
     im1, im2 = (np.array(Image.open(path).convert("RGB").resize((im_size,im_size),Image.LANCZOS), dtype=np.float)/255 for path in (path1, path2))
     files_basename = '_'.join([basename(path).split('.')[0] for path in (path1, path2)])
     return im1, im2, files_basename
